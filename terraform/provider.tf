a29bbf5425be4080702b95c371e4e50c9d9fe224
RaytraceSG <azmi.maniku@gmail.com>
2024-11-06 21:03:20 +0900

    add version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    archive = {
+      source = "hashicorp/data"
+      version = "~> 2.6"
+    }
   }
 }
 
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "archive" {
+
 }
